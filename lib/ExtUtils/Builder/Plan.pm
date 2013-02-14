668b0b0049fda92000ba65a97f291c741bbef1ec
Leon Timmermans <fawaka@gmail.com>
2013-02-14 01:55:07 +0100

    Split off Role::Dependency from Plan

--- lib/ExtUtils/Builder/Plan.pm
+++ lib/ExtUtils/Builder/Plan.pm
@@ -2,23 +2,7 @@ package ExtUtils::Builder::Plan;
 
 use Moo;
 
-with 'ExtUtils::Builder::Role::Action::Composite';
-
-has target => (
-	is       => 'ro',
-	required => 1,
-);
-
-has _dependencies => (
-	is       => 'ro',
-	required => 1,
-	init_arg => 'dependencies',
-);
-
-sub sources {
-	my $self = shift;
-	return @{ $self->_sources };
-}
+with qw/ExtUtils::Builder::Role::Dependency ExtUtils::Builder::Role::Action::Composite/;
 
 1;
 
