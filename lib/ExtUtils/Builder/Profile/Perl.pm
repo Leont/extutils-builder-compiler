e96717ae5506aa7294f76a38cf76317740bffc4a
Leon Timmermans <fawaka@gmail.com>
2013-02-14 01:42:15 +0100

    Improve portability of libperl linking

--- lib/ExtUtils/Builder/Profile/Perl.pm
+++ lib/ExtUtils/Builder/Profile/Perl.pm
@@ -8,15 +8,21 @@ use File::Spec::Functions qw/catdir/;
 
 sub _get_var {
 	my ($config, $opts, $key) = @_;
+	return delete $opts->{$key} || $config->get($key);
+}
+
+sub _split_var {
+	my ($config, $opts, $key) = @_;
 	return delete $opts->{$key} || [ split_like_shell($config->get($key)) ];
 }
 
 sub process_compiler {
 	my ($class, $compiler, $opts) = @_;
 	my $config = delete $opts->{config};
-	$compiler->add_include_dirs([ catdir($config->get('archlibexp'), 'CORE') ], ranking => sub { $_[0] + 1 });
-	$compiler->add_argument(ranking => 60, value => _get_var($config, $opts, 'ccflags'));
-	$compiler->add_argument(ranking => 65, value => _get_var($config, $opts, 'optimize'));
+	my $incdir = catdir(_get_var($config, $opts, 'archlibexp'), 'CORE');
+	$compiler->add_include_dirs([ $incdir ], ranking => sub { $_[0] + 1 });
+	$compiler->add_argument(ranking => 60, value => _split_var($config, $opts, 'ccflags'));
+	$compiler->add_argument(ranking => 65, value => _split_var($config, $opts, 'optimize'));
 	return;
 }
 
@@ -25,7 +31,7 @@ my $rpath_regex = qr/ ( (?<! \w ) (?: -Wl,-R | -Wl,-rpath | -R\ ? ) \S+ ) /x;
 sub process_linker {
 	my ($class, $linker, $opts) = @_;
 	my $config = delete $opts->{config};
-	$linker->add_argument(ranking => 60, value => _get_var($config, $opts, 'ldflags'));
+	$linker->add_argument(ranking => 60, value => _split_var($config, $opts, 'ldflags'));
 	if ($linker->export eq 'some') {
 		$linker->add_option_filter(sub {
 			my ($self, $from, $to, %opts) = @_;
@@ -34,11 +40,17 @@ sub process_linker {
 		});
 	}
 	if ($linker->type eq 'executable' or $linker->type eq 'shared-library') {
-		my ($libperl, $so) = map { $opts->{$_} || $config->get($_) } qw/libperl so/;
-		my ($lib) = $libperl =~ / \A (?:lib)? ( perl \w* ) \. $so \z /msx;
-		$linker->add_libraries([$lib]);
-		$linker->add_library_dirs([ catdir($config->get('archlibexp'), 'CORE')]);
-		$linker->add_argument(ranking => 80, value => _get_var($config, $opts, 'perllibs'));
+		if (_get_var($config, $opts, 'osname') eq 'MSWin32') {
+			$linker->add_argument(_get_var($config, $opts, 'libperl'));
+		} else {
+			my ($libperl, $libext, $so) = map { _get_var($config, $opts, $_) } qw/libperl lib_ext so/;
+			my ($lib) = $libperl =~ / \A (?:lib)? ( perl \w* ) (?: \. $so | $libext) \b /msx;
+			$linker->add_libraries([$lib]);
+		}
+
+		my $libdir = catdir(_get_var($config, $opts, 'archlibexp'), 'CORE');
+		$linker->add_library_dirs([ $libdir ]);
+		$linker->add_argument(ranking => 80, value => _split_var($config, $opts, 'perllibs'));
 	}
 	if ($linker->type eq 'executable') {
 		my $rpath = $opts->{rpath} || [ split_like_shell($config->get('ccdlflags') =~ $rpath_regex) ];
