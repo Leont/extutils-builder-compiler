99e926bcec0b80ea331ee3ef59cf25193aabbb77
Leon Timmermans <fawaka@gmail.com>
2024-06-16 18:17:31 +0200

    Reorder libperl argument on Windows

--- lib/ExtUtils/Builder/Profile/Perl.pm
+++ lib/ExtUtils/Builder/Profile/Perl.pm
@@ -44,7 +44,7 @@ sub process_linker {
 	my $os = _get_var($config, $opts, 'osname');
 	if ($linker->type eq 'executable' or $linker->type eq 'shared-library' or ($linker->type eq 'loadable-object' and $needs_relinking{$os})) {
 		if ($os eq 'MSWin32') {
-			$linker->add_argument(value => _split_var($config, $opts, 'libperl'));
+			$linker->add_argument(value => _split_var($config, $opts, 'libperl', ranking => $linker->default_library_ranking - 1));
 		}
 		else {
 			my ($libperl, $libext, $so) = map { _get_var($config, $opts, $_) } qw/libperl lib_ext so/;
